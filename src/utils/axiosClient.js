8b73d1853493a42767bda7a028b9b898189de269
Amey Ranaware <139114740+Ameysr@users.noreply.github.com>
2025-07-26 19:09:04 +0530

    Update axiosClient.js

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://codex-backend-psi.vercel.app/',
+    baseURL: 'https://codex-backend-psi.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
